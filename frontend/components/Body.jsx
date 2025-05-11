bfda96afe2a3bd1f73342e2254531da97c5c2834
Raunakaga2004 <raunakaga12@gmail.com>
2025-05-11 13:07:19 +0530

    basic structure of program done

--- frontend/components/Body.jsx
+++ frontend/components/Body.jsx
@@ -1,3 +1,18 @@
+import { ExampleEditor } from "./ExampleEditor";
+
 export default function Body(){
-    
+    return (<div>
+        {/* Code Editor */}
+        <ExampleEditor/>
+
+        {/* Output and Input Screen */}
+        <div>
+            {/* Input Screen */}
+
+            {/* Output Screen */}
+            <div>
+                Output
+            </div>
+        </div>
+    </div>)
 }
