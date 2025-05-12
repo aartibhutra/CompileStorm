4a6e5e6d1ff23eea82d880c2e57e07c66564dd99
Raunakaga2004 <raunakaga12@gmail.com>
2025-05-12 12:31:53 +0530

    added forgot password feature in sign in

--- backend/routes/compile.js
+++ backend/routes/compile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {signup, signin, signout, userDetails, runCode} = require("../controllers/compile")
+const {signup, signin, signout, userDetails, runCode, forgotPassword, verifyOtp, resetPassword} = require("../controllers/compile")
 const verifyToken = require('../middleware/verifytoken');
 // const { verify } = require('jsonwebtoken');
 
@@ -16,5 +16,13 @@ router.get('/userDetails', verifyToken,  userDetails);
 // EndPoints
 router.post('/runCode', verifyToken, runCode)
 
+//forgot-password
+router.post("/forgotpassword", forgotPassword)
+
+//verify-otp
+router.post("/verifyotp", verifyOtp)
+
+//reset password
+router.post("/resetpassword", resetPassword)
 
 module.exports = router;
