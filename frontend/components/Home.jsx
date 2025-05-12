5e756a2875453b79d46c4d5c4a8975eaaa06d732
aartibhutra <aartimaheshwari160@gmail.com>
2025-05-12 10:29:37 +0530

    add design

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -2,7 +2,7 @@ import Header from "./Header"
 import Body from "./Body"
 
 export default function Home(){
-    return (<div className=" flex bg-gradient-to-r from-zinc-400 to-zinc-700 h-screen">
+    return (<div className=" flex bg-gradient-to-r from-zinc-700 to-zinc-500 h-screen">
         <Header/>
         <Body/>
     </div>)
