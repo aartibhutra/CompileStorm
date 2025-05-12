b4719a806e6b888041bcbfe952cc74e8488eada8
Raunakaga2004 <raunakaga12@gmail.com>
2025-05-12 15:46:26 +0530

    profile guest execute code

--- frontend/components/Body.jsx
+++ frontend/components/Body.jsx
@@ -1,17 +1,18 @@
 // import { useRecoilValue } from "recoil";
 import { MonEditor } from "./MonEditor";
-import { useState } from "react";
+import {  useState } from "react";
 // import { outputSt } from "../store/output";
 
 
-export default function Body(){
+export default function Body({user}){
     const [op , setOp] =  useState("...")
+
     return (<div className="flex flex-col md:flex-row w-full h-screen p-4 gap-4 bg-zinc-900 text-white">
 
         {/* Code Editor */}
         <div className="flex-1 flex flex-col gap-4 overflow-hidden">
           <div className="flex-1 bg-zinc-800 rounded-lg shadow-inner p-2 overflow-hidden">
-          <MonEditor setOp={setOp}/>
+          <MonEditor setOp={setOp} user = {user}/>
           </div>
         </div>
         {/* Output and Input Screen */}
